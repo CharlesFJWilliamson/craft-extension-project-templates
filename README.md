4255da4e003d9dae4d351c8744e33ad90a0491ce
Charles <charles@stairwaylearning.com>
2021-12-02 22:37:58 +0000

    Initial Commit - table of contents for Craft

--- README.md
+++
@@ -1,3 +0,0 @@
-# Craft eXtension template projects
-
-To learn more about Craft eXtensions, visit the [Craft X developer portal](https://developer.craft.do) our [project template tutorials](https://documentation.developer.craft.do/extensions/tools/project-templates).
--- typescript-react/src/app.tsx
+++ typescript-react/src/app.tsx
@@ -1,6 +1,6 @@
-import * as React from "react"
-import * as ReactDOM from 'react-dom'
-import craftXIconSrc from "./craftx-icon.png"
+import { CraftTextBlockInsert } from "@craftdocs/craft-extension-api";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 
 const App: React.FC<{}> = () => {
   const isDarkMode = useCraftDarkMode();
@@ -13,36 +13,75 @@ const App: React.FC<{}> = () => {
     }
   }, [isDarkMode]);
 
-  return <div style={{
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  }}>
-    <img className="icon" src={craftXIconSrc} alt="CraftX logo" />
-    <button className={`btn ${isDarkMode ? "dark" : ""}`} onClick={insertHelloWorld}>
-      Hello world!
-    </button>
-  </div>;
-}
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <button
+        className={`btn ${isDarkMode ? "dark" : ""}`}
+        onClick={addTableOfContent}
+      >
+        Add Table of Content
+      </button>
+    </div>
+  );
+};
 
 function useCraftDarkMode() {
   const [isDarkMode, setIsDarkMode] = React.useState(false);
 
   React.useEffect(() => {
-    craft.env.setListener(env => setIsDarkMode(env.colorScheme === "dark"));
+    craft.env.setListener((env) => setIsDarkMode(env.colorScheme === "dark"));
   }, []);
 
   return isDarkMode;
 }
 
-function insertHelloWorld() {
-  const block = craft.blockFactory.textBlock({
-    content: "Hello world!"
+async function addTableOfContent() {
+  const result = await craft.dataApi.getCurrentPage();
+
+  if (result.status !== "success") {
+    throw new Error(result.message);
+  }
+
+  const pageBlock = result.data;
+  const pageId = pageBlock.id;
+  let textToIncludeInContents: string[] = [];
+  const pageContent = pageBlock.subblocks;
+
+  pageContent.map((block, index) => {
+    if (
+      block.type === "textBlock" &&
+      (block.style.textStyle === "title" ||
+        block.style.textStyle === "subtitle" ||
+        block.style.textStyle === "heading")
+    ) {
+      textToIncludeInContents.push(block.content.map((x) => x.text).join());
+    }
   });
 
-  craft.dataApi.addBlocks([block]);
+  let blockObjectsToInsert: CraftTextBlockInsert[] = [];
+  textToIncludeInContents.map((textToInclude, index) => {
+    blockObjectsToInsert.push(
+      craft.blockFactory.textBlock({
+        content: textToInclude,
+        listStyle: {
+          type: "bullet",
+        },
+      })
+    );
+  });
+
+  craft.dataApi.addBlocks(
+    blockObjectsToInsert,
+    craft.location.indexLocation(pageId, 0)
+  );
 }
 
 export function initApp() {
-  ReactDOM.render(<App />, document.getElementById('react-root'))
+  ReactDOM.render(<App />, document.getElementById("react-root"));
 }
