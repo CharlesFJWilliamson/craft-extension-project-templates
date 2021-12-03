b911fbe90463be405cc53a6ffde63517d617ca37
Charles <charles@stairwaylearning.com>
2021-12-03 00:16:45 +0000

    Updated to show table of contents in the sidebar

--- typescript-react/src/app.tsx
+++ typescript-react/src/app.tsx
@@ -13,6 +13,55 @@ const App: React.FC<{}> = () => {
     }
   }, [isDarkMode]);
 
+  const [tableOfContents, updateTableOfContentsState] = React.useState<
+    string[]
+  >([]);
+
+  const addTableOfContent = async (addToDocument: boolean) => {
+    const result = await craft.dataApi.getCurrentPage();
+
+    if (result.status !== "success") {
+      throw new Error(result.message);
+    }
+
+    const pageBlock = result.data;
+    const pageId = pageBlock.id;
+    let textToIncludeInContents: string[] = [];
+    const pageContent = pageBlock.subblocks;
+
+    pageContent.map((block, index) => {
+      if (
+        block.type === "textBlock" &&
+        (block.style.textStyle === "title" ||
+          block.style.textStyle === "subtitle" ||
+          block.style.textStyle === "heading")
+      ) {
+        textToIncludeInContents.push(block.content.map((x) => x.text).join());
+      }
+    });
+
+    updateTableOfContentsState(textToIncludeInContents);
+
+    if (addToDocument) {
+      let blockObjectsToInsert: CraftTextBlockInsert[] = [];
+      textToIncludeInContents.map((textToInclude, index) => {
+        blockObjectsToInsert.push(
+          craft.blockFactory.textBlock({
+            content: textToInclude,
+            listStyle: {
+              type: "bullet",
+            },
+          })
+        );
+      });
+
+      craft.dataApi.addBlocks(
+        blockObjectsToInsert,
+        craft.location.indexLocation(pageId, 0)
+      );
+    }
+  };
+
   return (
     <div
       style={{
@@ -21,12 +70,22 @@ const App: React.FC<{}> = () => {
         alignItems: "center",
       }}
     >
+      <h3 style={{ color: "white" }}>Table of Contents</h3>
       <button
         className={`btn ${isDarkMode ? "dark" : ""}`}
-        onClick={addTableOfContent}
+        onClick={() => addTableOfContent(true)}
       >
-        Add Table of Content
+        Add to Note
       </button>
+      <button
+        className={`btn ${isDarkMode ? "dark" : ""}`}
+        onClick={() => addTableOfContent(false)}
+      >
+        Show in Sidebar
+      </button>
+      <ul style={{ color: "white" }}>
+        {tableOfContents && tableOfContents.map((value) => <li>{value}</li>)}
+      </ul>
     </div>
   );
 };
@@ -39,47 +98,6 @@ function useCraftDarkMode() {
   }, []);
 
   return isDarkMode;
-}
-
-async function addTableOfContent() {
-  const result = await craft.dataApi.getCurrentPage();
-
-  if (result.status !== "success") {
-    throw new Error(result.message);
-  }
-
-  const pageBlock = result.data;
-  const pageId = pageBlock.id;
-  let textToIncludeInContents: string[] = [];
-  const pageContent = pageBlock.subblocks;
-
-  pageContent.map((block, index) => {
-    if (
-      block.type === "textBlock" &&
-      (block.style.textStyle === "title" ||
-        block.style.textStyle === "subtitle" ||
-        block.style.textStyle === "heading")
-    ) {
-      textToIncludeInContents.push(block.content.map((x) => x.text).join());
-    }
-  });
-
-  let blockObjectsToInsert: CraftTextBlockInsert[] = [];
-  textToIncludeInContents.map((textToInclude, index) => {
-    blockObjectsToInsert.push(
-      craft.blockFactory.textBlock({
-        content: textToInclude,
-        listStyle: {
-          type: "bullet",
-        },
-      })
-    );
-  });
-
-  craft.dataApi.addBlocks(
-    blockObjectsToInsert,
-    craft.location.indexLocation(pageId, 0)
-  );
 }
 
 export function initApp() {
